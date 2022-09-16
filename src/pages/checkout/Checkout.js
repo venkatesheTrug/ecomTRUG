0afe34d13dd312a545a6ebe83910802cc702a7b5
Venkatesh Guttula <venky@Venkateshs-MacBook-Pro.local>
2022-09-17 01:37:30 +0530

    push to heroku

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -42,7 +42,7 @@ const Checkout = () => {
     //https://eshop-react-firebase.herokuapp.com/create-payment-intent
     // Create PaymentIntent as soon as the page loads
     // fetch("https://eshop-react-firebase.herokuapp.com/create-payment-intent", {
-    fetch("http://localhost:4242/create-payment-intent", {
+    fetch("https://etrug-test-app.herokuapp.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
