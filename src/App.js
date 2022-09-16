7dddb206563c771ad26641a2182bd3dd08751b9e
Venkatesh Guttula <venky@Venkateshs-MacBook-Pro.local>
2022-09-17 01:51:10 +0530

    push to heroku after env variables

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 // Pages
 import { Home, Contact, Login, Register, Reset, Admin } from "./pages";
-// Components
+// Components.
 import { Header, Footer } from "./components";
 import AdminOnlyRoute from "./components/adminOnlyRoute/AdminOnlyRoute";
 import ProductDetails from "./components/product/productDetails/ProductDetails";
